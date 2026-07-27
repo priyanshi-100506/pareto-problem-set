0b0d4f9bdf39a7d8f480568c47ffe2fa0c1eef0c
priyanshi-100506 <priyanshicshah@gmail.com>
2026-07-27 10:43:40 +0530

    Add characterReplacement method for longest substring
    Implement character replacement algorithm to find the length of the longest substring with repeating characters after at most k replacements.

---
+++ 424. Longest Repeating Character Replacement.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count=defaultdict(int)
+        left=0
+        maxFreq=0
+        longest=0
+        for right in range(len(s)):
+            count[s[right]]+=1
+            maxFreq=max(maxFreq,count[s[right]])
+            while (right-left+1)-maxFreq>k:
+                count[s[left]]-=1
+                left+=1
+            longest=max(longest,right-left+1)
+
+        return longest

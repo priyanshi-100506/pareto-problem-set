4f913c9e2ff202ef166663ce834481e371fd6ee9
priyanshi-100506 <priyanshicshah@gmail.com>
2026-08-03 09:43:00 +0530

    Add maxDepth function using DFS and BFS methods
    Implemented both recursive and iterative methods to calculate the maximum depth of a binary tree.

---
+++ 104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#Recursive DFS Method
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+#Iterative BFS Method
+# Definition for a binary tree node.
+from collections import deque
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level=0
+        queue=deque([root])
+        while queue:
+            for i in range(len(queue)):
+                node=queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            level+=1
+        return level
+        

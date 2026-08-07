7fc8014a6b9931a1f99d1a847996ac35fe06ba19
priyanshi-100506 <priyanshicshah@gmail.com>
2026-08-07 13:39:43 +0530

    Implement goodNodes function to count good nodes

---
+++ 1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node,max_val):
+            if not node:
+                return 0
+            is_good=1 if node.val>=max_val else 0
+            return is_good+dfs(node.left,max(max_val,node.val))+dfs(node.right,max(max_val,node.val))
+        return dfs(root,-float('inf'))
